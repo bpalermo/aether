6b5c7e5e74d694e992a5485323b932f508e86c4b
Bruno Palermo <bruno.palermo@superbid.net>
2026-02-01 12:48:49 -0300

    reduce cognitive complexity

--- registrar/pkg/server/server.go
+++ registrar/pkg/server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"fmt"
-	"io"
 	"net"
 	"sync"
 
@@ -13,6 +12,9 @@ import (
 	"github.com/go-logr/logr"
 	protovalidate_middleware "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/protovalidate"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
 )
 
 type RegisterServer struct {
@@ -22,28 +24,38 @@ type RegisterServer struct {
 
 	log logr.Logger
 
-	grpcServer *grpc.Server
-	listener   net.Listener
+	grpcServer   *grpc.Server
+	healthServer *health.Server
+	listener     net.Listener
 
 	subscribers   map[string]registrarv1.RegistrarService_SubscribeServer
 	subscribersMu sync.RWMutex
-	clients       map[string]*actor.PID // key: address value: *pid
+	clients       map[string]*actor.PID
 }
 
 func NewRegisterServer(cfg *RegisterServerConfig, log logr.Logger) (*RegisterServer, error) {
 	validator, _ := protovalidate.New()
 
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(protovalidate_middleware.UnaryServerInterceptor(validator)),
+	)
+
+	healthServer := health.NewServer()
+	grpc_health_v1.RegisterHealthServer(grpcServer, healthServer)
+
+	// Register reflection service
+	reflection.Register(grpcServer)
+
 	return &RegisterServer{
-		registrarv1.UnimplementedRegistrarServiceServer{},
-		cfg,
-		log.WithName("register-server"),
-		grpc.NewServer(
-			grpc.UnaryInterceptor(protovalidate_middleware.UnaryServerInterceptor(validator)),
-		),
-		nil,
-		make(map[string]registrarv1.RegistrarService_SubscribeServer),
-		sync.RWMutex{},
-		make(map[string]*actor.PID),
+		UnimplementedRegistrarServiceServer: registrarv1.UnimplementedRegistrarServiceServer{},
+		cfg:                                 cfg,
+		log:                                 log.WithName("register-server"),
+		grpcServer:                          grpcServer,
+		healthServer:                        healthServer,
+		listener:                            nil,
+		subscribers:                         make(map[string]registrarv1.RegistrarService_SubscribeServer),
+		subscribersMu:                       sync.RWMutex{},
+		clients:                             make(map[string]*actor.PID),
 	}, nil
 }
 
@@ -56,6 +68,7 @@ func (rs *RegisterServer) Start(errCh chan<- error) error {
 	rs.listener = listener
 
 	registrarv1.RegisterRegistrarServiceServer(rs.grpcServer, rs)
+	rs.setHealthStatus(grpc_health_v1.HealthCheckResponse_SERVING)
 
 	go func() {
 		if err := rs.grpcServer.Serve(listener); err != nil {
@@ -66,86 +79,31 @@ func (rs *RegisterServer) Start(errCh chan<- error) error {
 	return nil
 }
 
-// Shutdown gracefully stops the gRPC server
 func (rs *RegisterServer) Shutdown(ctx context.Context) error {
-	if rs.grpcServer != nil {
-		stopped := make(chan struct{})
-		go func() {
-			rs.grpcServer.GracefulStop()
-			close(stopped)
-		}()
+	rs.setHealthStatus(grpc_health_v1.HealthCheckResponse_NOT_SERVING)
 
-		select {
-		case <-stopped:
-			// Graceful stop completed
-			rs.log.V(1).Info("gRPC server graceful stop completed")
-		case <-ctx.Done():
-			// Force stop on context timeout
-			rs.log.V(1).Info("gRPC server forced stop due to timeout")
-			rs.grpcServer.Stop()
-			return ctx.Err()
-		}
+	if rs.grpcServer == nil {
+		return nil
 	}
 
-	return nil
-}
-
-func (rs *RegisterServer) Subscribe(stream registrarv1.RegistrarService_SubscribeServer) (err error) {
-	in, err := stream.Recv()
-	if err != nil {
-		return err
-	}
-
-	proxyId := in.ProxyId
-	rs.log.V(1).Info("new stream received", "proxyId", proxyId)
-
-	// Register this subscriber
-	rs.subscribersMu.Lock()
-	rs.subscribers[proxyId] = stream
-	rs.subscribersMu.Unlock()
-
-	// Cleanup on exit
-	defer func() {
-		rs.log.V(1).Info("unsubscribing", "proxyId", proxyId)
-		rs.subscribersMu.Lock()
-		delete(rs.subscribers, proxyId)
-		rs.subscribersMu.Unlock()
-		rs.log.Info("unsubscribed", "proxyId", proxyId)
+	stopped := make(chan struct{})
+	go func() {
+		rs.grpcServer.GracefulStop()
+		close(stopped)
 	}()
 
-	// Handle incoming messages
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-		rs.log.V(1).Info("incoming request", "proxyId", req.ProxyId)
-
-		// Broadcast to all other subscribers
-		rs.handleRequest(req)
+	select {
+	case <-stopped:
+		rs.log.V(1).Info("gRPC server graceful stop completed")
+		return nil
+	case <-ctx.Done():
+		rs.log.V(1).Info("gRPC server forced stop due to timeout")
+		rs.grpcServer.Stop()
+		return ctx.Err()
 	}
 }
 
-func (rs *RegisterServer) handleRequest(req *registrarv1.SubscribeRequest) {
-
-}
-
-func (rs *RegisterServer) broadcast(senderID string) {
-	rs.subscribersMu.RLock()
-	defer rs.subscribersMu.RUnlock()
-
-	for proxyID, _ := range rs.subscribers {
-		if proxyID == senderID {
-			rs.log.V(1).Info("skipping sender", "proxyID", proxyID)
-			continue // Do not send it to sender
-		}
-
-		rs.log.V(1).Info("sending event", "proxyID", proxyID)
-		//if err := subscriberStream.Send(msg); err != nil {
-		//	rs.log.Error(err, "failed to send event", "proxyID", proxyID)
-		//}
-	}
+func (rs *RegisterServer) setHealthStatus(status grpc_health_v1.HealthCheckResponse_ServingStatus) {
+	rs.healthServer.SetServingStatus("", status)
+	rs.healthServer.SetServingStatus(registrarv1.RegistrarService_ServiceDesc.ServiceName, status)
 }
